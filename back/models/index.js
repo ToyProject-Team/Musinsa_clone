4b56a118fd870b4bb5521cf8398d3f6759d77ed2
이민서 <sola2014@naver.com>
2022-07-25 13:37:25 +0900

    feat

---
+++ back/models/index.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+const db = {};
+
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env];
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+Object.keys(db).forEach((modelName) => {
+  console.log(modelName);
+  db[modelName].init(sequelize);
+});
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
