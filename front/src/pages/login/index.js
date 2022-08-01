a7991456e8e97422b2ca223f87e5a6dd34467c11
Whyukim <devhyukim@gmail.com>
2022-08-01 16:49:31 +0900

    style loginPage

--- front/src/pages/login/index.js
+++ front/src/pages/login/index.js
@@ -1,8 +1,46 @@
 import React from 'react';
-import { Hello } from './styles';
+import {
+	Container,
+	LoginSection,
+	Header,
+	Form,
+	Input,
+	Button,
+	AutoLogin,
+	FindLogin,
+	KakaoLogIn,
+	LinkContainer,
+	Signup,
+} from 'pages/login/styles';
 
 const LogIn = () => {
-	return <Hello>로그인페이지</Hello>;
+	return (
+		<Container>
+			<LoginSection>
+				<Header>로그인</Header>
+				<Form>
+					<div>
+						<Input>아이디</Input>
+						<Input>비밀번호</Input>
+						<Button>로그인</Button>
+					</div>
+					<div>
+						<AutoLogin></AutoLogin>
+						<FindLogin>
+							<span>아이디 찾기</span>
+							<span>비밀번호 찾기</span>
+						</FindLogin>
+					</div>
+					<div>
+						<KakaoLogIn></KakaoLogIn>
+					</div>
+				</Form>
+				<LinkContainer>
+					<Signup href="/signup">회원가입</Signup>
+				</LinkContainer>
+			</LoginSection>
+		</Container>
+	);
 };
 
 export default LogIn;
--- front/src/pages/login/styles.js
+++ front/src/pages/login/styles.js
@@ -1,5 +1,56 @@
 import styled from '@emotion/styled';
 
-export const Hello = styled.div({
-	color: 'hotpink',
-});
+export const Container = styled.div`
+	height: 100vh;
+	background-color: #f1f1f1;
+	overflow: hidden;
+	font-family: HelveticaNeue;
+`;
+
+export const LoginSection = styled.form`
+	max-width: 400px;
+	height: 100vh;
+	margin: 0 auto;
+	background-color: #fff;
+	padding-top: 62px;
+`;
+
+export const Header = styled.h2`
+	display: flex;
+	justify-content: center;
+	align-content: center;
+	height: 50px;
+	font-size: 16px;
+	line-height: 1.5;
+	font-weight: 600;
+`;
+
+export const Form = styled.div`
+	color: hotpink;
+`;
+
+export const Input = styled.div`
+	color: hotpink;
+`;
+
+export const Button = styled.div`
+	color: hotpink;
+`;
+
+export const AutoLogin = styled.div`
+	color: hotpink;
+`;
+
+export const FindLogin = styled.div`
+	color: hotpink;
+`;
+
+export const KakaoLogIn = styled.div`
+	color: hotpink;
+`;
+export const LinkContainer = styled.div`
+	color: hotpink;
+`;
+export const Signup = styled.div`
+	color: hotpink;
+`;
