42d3e92be0ba3ecc4d4003eb5b3574c31a5194ba
Whyukim <devhyukim@gmail.com>
2022-08-06 03:52:22 +0900

    style mypage

---
+++ front/src/components/Mypage/styles.js
@@ -0,0 +1,65 @@
+import { Link } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+export const Header = styled.div`
+	position: relative;
+	background: black;
+	padding: 20px 20px 40px;
+`;
+
+export const MypageTitle = styled.h2`
+	font-size: 24px;
+	line-height: 52px;
+	color: white;
+	margin-bottom: 10px;
+`;
+
+export const Profile = styled.dl`
+	padding-left: 45px;
+	&::after {
+		display: block;
+		content: '';
+		clear: both;
+	}
+`;
+
+export const PRFL_IMAGE = styled.dt`
+	float: left;
+	width: 160px;
+	height: 160px;
+	border-radius: 50%;
+	background: white;
+`;
+
+export const NickName = styled.dd`
+	display: inline-block;
+	padding-left: 50px;
+	padding-top: 30px;
+	color: white;
+	font-weight: bold;
+	font-size: 58px;
+	line-height: 87px;
+`;
+
+export const Navi = styled.ul`
+	position: absolute;
+	top: 20px;
+	right: 0;
+	color: white;
+	padding: 20px;
+	&::after {
+		display: block;
+		content: '';
+		clear: both;
+	}
+`;
+
+export const NaviList = styled.li`
+	float: left;
+	margin-left: 32px;
+`;
+
+export const StyleLink = styled(Link)`
+	color: #fff;
+	text-decoration: none;
+`;
