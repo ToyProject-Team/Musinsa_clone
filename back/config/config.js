4834f5548d64c8f039126bc47cc447f1947cf6d4
이민서 <sola2014@naver.com>
2022-08-12 14:20:51 +0900

    refactor: 수정

--- back/config/config.js
+++ back/config/config.js
@@ -4,21 +4,21 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: "minseo",
+    username: "plz",
     password: process.env.DB_PASSWORD,
     database: "musinsaApp",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
-    username: "minseo",
+    username: "plz",
     password: process.env.DB_PASSWORD,
     database: "musinsaApp",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
-    username: "minseo",
+    username: "plz",
     password: process.env.DB_PASSWORD,
     database: "musinsaApp",
     host: "127.0.0.1",
