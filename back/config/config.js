5517742792956091b296d9c738dfb930ea1a2a92
이민서 <sola2014@naver.com>
2022-08-12 15:01:56 +0900

    das

--- back/config/config.js
+++ back/config/config.js
@@ -4,21 +4,21 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: "root",
+    username: NODE_ENV==production? 'plz ': "root",
     password: process.env.DB_PASSWORD,
     database: "musinsaApp",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
-    username: "root",
+    username: NODE_ENV==production? 'plz ': "root",
     password: process.env.DB_PASSWORD,
     database: "musinsaApp",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
-    username: "root",
+    username: NODE_ENV==production? 'plz ': "root",
     password: process.env.DB_PASSWORD,
     database: "musinsaApp",
     host: "127.0.0.1",
