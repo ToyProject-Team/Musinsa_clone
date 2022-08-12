16211965dca799cf4b72de6b25fc03751c8e68ee
이민서 <sola2014@naver.com>
2022-08-12 15:03:57 +0900

    adas

--- back/config/config.js
+++ back/config/config.js
@@ -4,21 +4,21 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: NODE_ENV==production? 'plz ': "root",
+    username: 'plz',
     password: process.env.DB_PASSWORD,
     database: "musinsaApp",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
-    username: NODE_ENV==production? 'plz ': "root",
+    username: 'plz',
     password: process.env.DB_PASSWORD,
     database: "musinsaApp",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
-    username: NODE_ENV==production? 'plz ': "root",
+    username: 'plz',
     password: process.env.DB_PASSWORD,
     database: "musinsaApp",
     host: "127.0.0.1",
