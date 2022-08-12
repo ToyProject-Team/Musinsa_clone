c8a55f62056af5c707c244f7ad5033f48c33c636
이민서 <sola2014@naver.com>
2022-08-12 16:07:13 +0900

    dads

--- back/config/config.js
+++ back/config/config.js
@@ -3,21 +3,21 @@ const dotenv = require("dotenv");
 dotenv.config();
 module.exports = {
   development: { 
-    username: process.env.NODE_ENV=='production'? 'root' : 'plz',
+    username: 'plz',
     password: process.env.DB_PASSWORD,
     database: "musinsaApp",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
-    username: process.env.NODE_ENV=='production'? 'root' : 'plz',
+    username: 'plz',
     password: process.env.DB_PASSWORD,
     database: "musinsaApp",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
-    username: process.env.NODE_ENV=='production'? 'root' : 'plz',
+    username: 'plz',
     password: process.env.DB_PASSWORD,
     database: "musinsaApp",
     host: "127.0.0.1",
