0a1f709bd24f74d8cb8598e82e9b2502c88b352f
이민서 <sola2014@naver.com>
2022-08-16 16:19:21 +0900

    addas

--- back/utils/redis.js
+++ back/utils/redis.js
@@ -1,5 +1,7 @@
 const { createClient } = require("redis");
+const dotenv = require('dotenv')
 
+dotenv.config()
 const redisClient = createClient({
   host: process.env.NODE_ENV === "production" ? "localhost:80" : "141.164.48.244",
   url: `redis://${process.env.REDIS_HOST}`,
