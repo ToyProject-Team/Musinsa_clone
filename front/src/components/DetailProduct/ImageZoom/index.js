2c56783e96543b8ba1221ddd5ea17a8374e95565
Whyukim <devhyukim@gmail.com>
2022-09-09 00:00:18 +0900

    Update 이미지 슬라이드 줌 기능

--- front/src/components/DetailProduct/ImageZoom/index.js
+++ front/src/components/DetailProduct/ImageZoom/index.js
@@ -1,36 +1,68 @@
 import React from 'react';
 import { useCallback } from 'react';
 import { useState } from 'react';
-import { ProductImgContainer, ProductImg, ProductImgZoom } from './styles';
+import {
+	ProductImgContainer,
+	ProductImg,
+	ProductImgZoomContainer,
+	ProductImgZoom,
+	Cursor,
+} from './styles';
 
 const ImageZoom = ({ img, alt }) => {
 	const [show, setShow] = useState(false);
+	const [mouseCursor, setMouseCursor] = useState({ x: 0, y: 0 });
+	const [mousePosition, setMousePosition] = useState({
+		left: 0,
+		top: 0,
+	});
 
-	const onClickZoomIn = useCallback(
-		e => {
-			console.log('in', e.target);
-			setShow(true);
-		},
-		[show],
-	);
+	const onClickZoomIn = useCallback(() => {
+		setShow(true);
+	}, [show]);
 
-	const onClickZoomOut = useCallback(
-		e => {
-			console.log('out', e.target);
-			setShow(false);
-		},
-		[show],
-	);
+	const onClickZoomOut = useCallback(() => {
+		setShow(false);
+	}, [show]);
+
+	const onMoseMoveZoom = useCallback(e => {
+		setMousePosition({
+			left: -e.pageX + 20 + 'px',
+			top: -e.pageY + 105 + 'px',
+		});
+		setMouseCursor({ x: e.pageX - 35, y: e.pageY - 120 });
+	}, []);
 
 	return (
 		<ProductImgContainer>
-			<ProductImg onClick={onClickZoomIn}>
+			<ProductImg onMouseMove={onMoseMoveZoom}>
+				{!show &&
+					mouseCursor.x < 486 &&
+					mouseCursor.y < 580 &&
+					mouseCursor.x > -18 &&
+					mouseCursor.y > -8 && (
+						<Cursor
+							onClick={onClickZoomIn}
+							show={show}
+							className="cursor"
+							style={{ position: 'absolute', left: mouseCursor.x, top: mouseCursor.y }}
+						></Cursor>
+					)}
 				<img src={img} alt={alt} />
 			</ProductImg>
 
-			<ProductImgZoom show={show} onClick={onClickZoomOut}>
-				hello
-			</ProductImgZoom>
+			<ProductImgZoomContainer show={show} onClick={onClickZoomOut} onMouseMove={onMoseMoveZoom}>
+				<ProductImgZoom show={show}>
+					<img src={img} alt={alt} style={{ left: mousePosition.left, top: mousePosition.top }} />
+				</ProductImgZoom>
+				{show && (
+					<Cursor
+						show={show}
+						className="cursor"
+						style={{ position: 'absolute', left: mouseCursor.x, top: mouseCursor.y }}
+					></Cursor>
+				)}
+			</ProductImgZoomContainer>
 		</ProductImgContainer>
 	);
 };
--- front/src/components/DetailProduct/ImageZoom/styles.js
+++ front/src/components/DetailProduct/ImageZoom/styles.js
@@ -30,20 +30,67 @@ export const ProductImgContainer = styled.div`
 `;
 
 export const ProductImg = styled.div`
+	opacity: ${props => (props.show ? 0 : 1)};
+	visibility: ${props => (props.show ? 'hidden' : 'visible')};
+	cursor: none;
+
 	img: {
 		width: '100%';
 	}
+`;
+
+export const ProductImgZoomContainer = styled.div`
+	position: absolute;
+	left: 0;
+	top: 0;
+	width: 200%;
+	height: 100%;
+	opacity: ${props => (props.show ? 1 : 0)};
+	visibility: ${props => (props.show ? 'visible' : 'hidden')};
+	transition: ${props => (props.show ? '0s' : '0.5s')};
+	z-index: 100;
+	overflow: hidden;
+	cursor: none;
 `;
 
 export const ProductImgZoom = styled.div`
 	position: absolute;
 	left: 0;
 	top: 0;
-	border: 3px solid red;
 	width: ${props => (props.show ? '200%' : '0px')};
 	height: ${props => (props.show ? '100%' : '0px')};
 	animation: ${props => (props.show ? ZoomIn : ZoomOut)} 0.3s ease-in;
 	opacity: ${props => (props.show ? 1 : 0)};
+	visibility: ${props => (props.show ? 'visible' : 'hidden')};
 	transition: 0.3s;
-	transition-delay: ${props => (props.show ? 0 : '0.2s')};
+	transition-delay: '${props => (props.show ? '0' : '0.2s')}';
+	overflow: hidden;
+
+	& > img {
+		position: absolute;
+		width: 100%;
+		height: 200%;
+	}
+`;
+
+export const Cursor = styled.div`
+	position: absolute;
+	left: 0;
+	top: 0;
+	width: 25px;
+	height: 25px;
+	/* background-color: rgba(255, 255, 255, 0.5); */
+	background-color: red;
+	z-index: 999;
+
+	&::before {
+		content: '${props => (props.show ? '-' : '+')}';
+		position: absolute;
+		top: 40%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		font-weight: 900;
+		font-size: 30px;
+		color: #828282;
+	}
 `;
