7b83970f7afa09e1d1fe9449ffd8365d98656804
이민서 <sola2014@naver.com>
2022-09-19 14:20:28 +0900

    fix: 장바구니 불필요한 데이터 전달 삭제

--- back/routes/shoppingBasket.js
+++ back/routes/shoppingBasket.js
@@ -22,7 +22,7 @@ router.get('/shoppingList', authJWT, async (req, res, next) => {
             //         }
             //     ]
             // },
-            joinTableAttributes: ["ProductId", "packingAmount", "packingSize"],
+            // joinTableAttributes: ["ProductId", "packingAmount", "packingSize"],
             attributes: ["id", "productTitle", "productPrice", "nonMemberPrice", "deliveryFrom", "deliveryWay","deliveryCompany"],
             include: [
                 {
