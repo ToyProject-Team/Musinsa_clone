faf902f1a45c18d34a9c8c3e5175e3230a6c7442
Whyukim <devhyukim@gmail.com>
2022-10-07 13:13:16 +0900

    update sidebar

--- front/src/layouts/Sidebar/index.js
+++ front/src/layouts/Sidebar/index.js
@@ -1,6 +1,6 @@
 import { useState, useCallback } from 'react';
 import { SContainer, SDiv } from './styles';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import { bigCategory, alpabet } from 'utils/bigCategory';
 import { smallCategory } from 'utils/smallCategory';
 import { HiOutlinePlusSm, HiOutlineMinusSm } from 'react-icons/hi';
@@ -8,91 +8,102 @@ import Cookies from 'js-cookie';
 import { SIDEBAR, useGlobalDispatch } from 'context/GlobalContext';
 
 const Sidebar = props => {
-	const dispatch = useGlobalDispatch();
-	// const [cancel, setCancel] = useState(true);
-	const [cancel, setCancel] = useState(Cookies.get('sideBarToggle') === 'false' ? false : true);
-	const [open, setOpen] = useState(Array.from({ length: bigCategory.length }, () => false));
+    const dispatch = useGlobalDispatch();
+    const location = useLocation();
+    const navigate = useNavigate();
+    // const [cancel, setCancel] = useState(true);
+    const [cancel, setCancel] = useState(Cookies.get('sideBarToggle') === 'false' ? false : true);
+    const [open, setOpen] = useState(Array.from({ length: bigCategory.length }, () => false));
 
-	// const navigate = useNavigate();
+    // const navigate = useNavigate();
 
-	//Main에 쿼리스트링 보내기
-	const sendSmallCate = (big, small) => {
-		// smallCate index가 0인 경우(전체) / 아닌경우
-		// smallCate index가 1이상인 경우에는 bigCateId가 이미 있는경우/없는경우
-		//이중 삼항연산자
-		small === 0
-			? props.setFilterVal(() => {
-					return { bigCategoryId: big + 1 };
-			  })
-			: props.filterVal.bigCategoryId - 1 === big
-			? props.setFilterVal(prev => {
-					return { ...prev, smallCategoryId: small };
-			  })
-			: props.setFilterVal(() => {
-					return { bigCategoryId: big + 1, smallCategoryId: small };
-			  });
-	};
+    //Main에 쿼리스트링 보내기
+    const sendSmallCate = (big, small) => {
+        // smallCate index가 0인 경우(전체) / 아닌경우
+        // smallCate index가 1이상인 경우에는 bigCateId가 이미 있는경우/없는경우
+        //이중 삼항연산자
 
-	const onClickCategory = useCallback(idx => {
-		setOpen(prev => [...prev].map((v, index) => (idx === index ? (v ? false : true) : false)));
-	}, []);
+        const { pathname } = location;
 
-	const onClickToggle = useCallback(() => {
-		setCancel(e => !e);
-		Cookies.set('sideBarToggle', !cancel);
-		console.log(cancel);
-		const payload = {
-			sideBar: !cancel,
-		};
-		dispatch({ type: SIDEBAR, payload });
-	}, [cancel]);
+        if (pathname !== '/detail') {
+            small === 0
+                ? props.setFilterVal(() => {
+                      return { bigCategoryId: big + 1 };
+                  })
+                : props.filterVal.bigCategoryId - 1 === big
+                ? props.setFilterVal(prev => {
+                      return { ...prev, smallCategoryId: small };
+                  })
+                : props.setFilterVal(() => {
+                      return { bigCategoryId: big + 1, smallCategoryId: small };
+                  });
+        } else {
+            return navigate(`/products?bigCategoryId=${big + 1}&smallCategoryId=${small}`);
+        }
+    };
 
-	return (
-		<SContainer>
-			<div onClick={onClickToggle} className={cancel ? 'toggle active' : 'toggle'}>
-				<span className="line"></span>
-				<span className="line"></span>
-				<span className="line"></span>
-			</div>
-			<SDiv className={cancel ? 'appear' : 'disappear'}>
-				<nav>
-					{bigCategory.map((big, idx) => (
-						<div
-							title="MeunContainer"
-							onClick={() => onClickCategory(idx)}
-							aria-expanded={open[idx]}
-							key={idx}
-						>
-							<div title="BigMenu" key={idx}>
-								{big}
-								<span>{alpabet[idx]}</span>
-								<span>{!open[idx] ? <HiOutlinePlusSm /> : <HiOutlineMinusSm />}</span>
-							</div>
+    const onClickCategory = useCallback(idx => {
+        setOpen(prev => [...prev].map((v, index) => (idx === index ? (v ? false : true) : false)));
+    }, []);
 
-							<ul
-								title="smallMenu"
-								aria-expanded={!open[idx]}
-								style={!open[idx] ? { paddingTop: '0' } : { paddTop: '20px' }}
-								onClick={e => e.stopPropagation()}
-							>
-								{smallCategory[idx].map((small, idex) => (
-									<li
-										key={idex}
-										onClick={() => {
-											sendSmallCate(idx, idex);
-										}}
-									>
-										<span key={idex}>{small}</span>
-										<span>{`(${idex + 1})`}</span>
-									</li>
-								))}
-							</ul>
-						</div>
-					))}
-				</nav>
-			</SDiv>
-		</SContainer>
-	);
+    const onClickToggle = useCallback(() => {
+        setCancel(e => !e);
+        Cookies.set('sideBarToggle', !cancel);
+        console.log(cancel);
+        const payload = {
+            sideBar: !cancel,
+        };
+        dispatch({ type: SIDEBAR, payload });
+    }, [cancel]);
+
+    return (
+        <SContainer>
+            <div onClick={onClickToggle} className={cancel ? 'toggle active' : 'toggle'}>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+            </div>
+            <SDiv className={cancel ? 'appear' : 'disappear'}>
+                <nav>
+                    {bigCategory.map((big, idx) => (
+                        <div
+                            title="MeunContainer"
+                            onClick={() => onClickCategory(idx)}
+                            aria-expanded={open[idx]}
+                            key={idx}
+                        >
+                            <div title="BigMenu" key={idx}>
+                                {big}
+                                <span>{alpabet[idx]}</span>
+                                <span>
+                                    {!open[idx] ? <HiOutlinePlusSm /> : <HiOutlineMinusSm />}
+                                </span>
+                            </div>
+
+                            <ul
+                                title="smallMenu"
+                                aria-expanded={!open[idx]}
+                                style={!open[idx] ? { paddingTop: '0' } : { paddTop: '20px' }}
+                                onClick={e => e.stopPropagation()}
+                            >
+                                {smallCategory[idx].map((small, idex) => (
+                                    <li
+                                        key={idex}
+                                        onClick={() => {
+                                            sendSmallCate(idx, idex);
+                                        }}
+                                    >
+                                        <span key={idex}>{small}</span>
+                                        <span>{`(${idex + 1})`}</span>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </nav>
+            </SDiv>
+        </SContainer>
+    );
 };
 
 export default Sidebar;
