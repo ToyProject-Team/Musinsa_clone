e498427ecbc21f12a617c88a92d6ea2c7d9e4074
2jun0 <soo28819@naver.com>
2022-10-12 14:08:09 +0900

    Update: /api/shoppingBasket/parchase #67

--- back/models/productSubTag.js
+++ back/models/productSubTag.js
@@ -1,3 +1,4 @@
+const { Transaction } = require('sequelize');
 const DataTypes = require('sequelize');
 const { Model } = DataTypes;
 
@@ -31,5 +32,25 @@ module.exports = class ProductSubTag extends Model {
     }
     static associate(db) {
         db.ProductSubTag.belongsTo(db.ProductMainTag);
+    }
+
+    /**
+     * 모델에서 개수를 지운다.
+     * ```js
+     * productSubTag = await productSubTag.reductAmount(10)
+     * ```
+     * @param {number} amount 지울 개수
+     * @param {DataTypes.Options} option
+     */
+    async reduceAmount(amount, option) {
+        if (this.amount < amount)
+            throw new Error(
+                `${amount}만큼 지울 수 없습니다. amount는 ${this.amount}보다 같거나 작아야 합니다.`,
+            );
+
+        if (amount <= this.amount) {
+            await this.update({ amount: this.amount - amount }, option);
+            return this.save(option);
+        }
     }
 };
