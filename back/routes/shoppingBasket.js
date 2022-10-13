865787c9d23b4f94e91930ea98bfef51ccaf67c6
2jun0 <soo28819@naver.com>
2022-10-13 13:23:56 +0900

    Update: /api/shoppingBasket/shoppingList에서 product안에 customcategory 추가

--- back/routes/shoppingBasket.js
+++ back/routes/shoppingBasket.js
@@ -9,6 +9,7 @@ const {
     sequelize,
     Order,
     Sequelize,
+    CustomCategory,
 } = require('../models');
 const authJWT = require('../utils/middlewares/authJWT');
 const {
@@ -62,7 +63,10 @@ router.get('/shoppingList', authJWT, async (req, res, next) => {
                             model: ProductImg,
                             attributes: ['src'],
                         },
-
+                        {
+                            model: CustomCategory,
+                            attributes: ['id', 'categoryName'],
+                        },
                         {
                             model: ProductMainTag,
                             attributes: ['name'],
