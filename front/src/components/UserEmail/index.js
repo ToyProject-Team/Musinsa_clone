62351572f0fc8c5e0daa4388e74b4c7ea5428ac9
Whyukim <devhyukim@gmail.com>
2022-10-26 18:01:58 +0900

    update

--- front/src/components/UserEmail/index.js
+++ front/src/components/UserEmail/index.js
@@ -4,43 +4,45 @@ import { LoginContainer } from './styles';
 import { ReactComponent as CancelIcon } from 'assets/svg/Cancel.svg';
 
 const UserEmail = ({ email, setEmail, onChangeEmail, placeholder, title, reg, setReg }) => {
-	const [signUpPage, setSignUpPage] = useState(
-		window.location.pathname === '/login' ? false : true,
-	);
+    const [signUpPage, setSignUpPage] = useState(
+        window.location.pathname === '/login' || '#/login' ? false : true,
+    );
 
-	// input clear button
-	const onClickClear = useCallback(() => {
-		setEmail('');
-		setReg(false);
-	}, [setEmail]);
+    // input clear button
+    const onClickClear = useCallback(() => {
+        setEmail('');
+        setReg(false);
+    }, [setEmail]);
 
-	return (
-		<>
-			<LoginContainer>
-				{title && (
-					<label>
-						아이디
-						<span>필수 입력</span>
-					</label>
-				)}
-				<div>
-					<input
-						className="email"
-						value={email}
-						onChange={onChangeEmail}
-						placeholder={placeholder}
-					/>
-					{email?.length > 0 && (
-						<button type="button" onClick={() => onClickClear()}>
-							<CancelIcon />
-						</button>
-					)}
-				</div>
-				{signUpPage && !reg && title && email.length > 0 && <p>아이디는 필수정보 입니다.</p>}
-				{signUpPage && !reg && !title && <p>답변은 필수정보 입니다.</p>}
-			</LoginContainer>
-		</>
-	);
+    return (
+        <>
+            <LoginContainer>
+                {title && (
+                    <label>
+                        아이디
+                        <span>필수 입력</span>
+                    </label>
+                )}
+                <div>
+                    <input
+                        className="email"
+                        value={email}
+                        onChange={onChangeEmail}
+                        placeholder={placeholder}
+                    />
+                    {email?.length > 0 && (
+                        <button type="button" onClick={() => onClickClear()}>
+                            <CancelIcon />
+                        </button>
+                    )}
+                </div>
+                {signUpPage && !reg && title && email.length > 0 && (
+                    <p>아이디는 필수정보 입니다.</p>
+                )}
+                {signUpPage && !reg && !title && <p>답변은 필수정보 입니다.</p>}
+            </LoginContainer>
+        </>
+    );
 };
 
 export default UserEmail;
